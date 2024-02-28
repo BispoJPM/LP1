88c19bf133333123a86cff7a6a5c509aac34c551
João Pedro <142633184+BispoJPM@users.noreply.github.com>
2024-02-28 19:54:49 -0300

    Update Exercicio3.java

--- Exercicio3.java
+++ Exercicio3.java
@@ -1,26 +1,40 @@
-package Ling_Prog;
 import java.util.Scanner;
 
-public class Exercicio3 {
-    public static void main(String[] args) {
-        Scanner notas = new Scanner(System.in);
-        float nota1;
-        float nota2;
-        float nota3;
+public class Exercicio5 {
 
-        System.out.println("Digite a primeira nota: ");
-        nota1 = notas.nextFloat();
+        public static void main(String[] args){
 
-        System.out.println("Digite a segunda nota: ");
-        nota2 = notas.nextFloat();
+        Scanner nota = new Scanner(System.in);
 
-        System.out.println("Digite a terceira nota: ");
-        nota3 = notas.nextFloat();
+        float P1;
+        float E1;
+        float E2;
+        float API;
+        float SUB;
+        float X;
+        float LP1; 
 
-        float media = nota1 + nota2 + nota3;
-        float media1 = media/= 3;
-
-        System.out.println("\n" +"A média desse aluno foi: " + media1 + "\n");
-
-    }
+        System.out.print("Digite a nota da prova: ");
+        P1 = nota.nextFloat();
+        System.out.print("Digite a nota da Atividade 1: ");
+        E1 = nota.nextFloat();
+        System.out.print("Digite a nota da Atividade 2: ");
+        E2 = nota.nextFloat();
+        System.out.print("Digite a nota da API: ");
+        API = nota.nextFloat();
+        System.out.print("Digite a nota da prova substitutiva: ");
+        SUB = nota.nextFloat();
+        System.out.println("Digite os pontos a mais X: ");
+        X = nota.nextFloat();
+        
+        LP1 = (float)((p1*0.6+(((e1+e2)/2)* 0.4)) * 0.5 + (Math.max(((p1*0.6+((e1+e2)/2)*0.4)-5.9), 0)/((p1*0.6+((e1+e2)/2)*0.4)-5.9))*(api*0.5)+x+(sub*0.2));
+        
+        if(lp1 > 10){
+            System.out.println("Execute o programa novamente e verifique as notas! (O resultado foi acima de 10,00)");
+        }else{
+            System.out.println("Sua média é " + lp1);
+        }
+        
+    }   
+    
 }
