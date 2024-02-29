f0eb99b79ab59f14fb70a04608c8580ccf94f91c
João Pedro <142633184+BispoJPM@users.noreply.github.com>
2024-02-28 21:14:41 -0300

    Ex3

--- Exercicio3.java
+++ Exercicio3.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 
-public class Exercicio3 {
+public class app {
         public static void main(String[] args){
-                
-        Scanner nota = new Scanner(System.in);
+
+            Scanner nota = new Scanner(System.in);
 
         float P1;
         float E1;
@@ -12,28 +12,34 @@ public class Exercicio3 {
         float SUB;
         float X;
         float LP1; 
+        float LPAPI;
+        float LPSUB;
+        float Teste;
 
+        
         System.out.print("Digite a nota da prova: ");
         P1 = nota.nextFloat();
         System.out.print("Digite a nota da Atividade 1: ");
         E1 = nota.nextFloat();
         System.out.print("Digite a nota da Atividade 2: ");
         E2 = nota.nextFloat();
-        System.out.print("Digite a nota da API: ");
-        API = nota.nextFloat();
-        System.out.print("Digite a nota da prova substitutiva: ");
-        SUB = nota.nextFloat();
         System.out.println("Digite os pontos a mais X: ");
         X = nota.nextFloat();
-        
-        LP1 = (float)((p1*0.6+(((e1+e2)/2)* 0.4)) * 0.5 + (Math.max(((p1*0.6+((e1+e2)/2)*0.4)-5.9), 0)/((p1*0.6+((e1+e2)/2)*0.4)-5.9))*(api*0.5)+x+(sub*0.2));
-        
-        if(lp1 > 10){
-            System.out.println("Execute o programa novamente e verifique as notas! (O resultado foi acima de 10,00)");
+
+        LP1 = (float) ((P1*0.6+((E1+E2)/2)*0.4)*0.5+X);
+            System.out.println(LP1);
+        Teste= (float) (Math.max(((P1*0.6+((E1+E2)/2)*0.4)-5.9),0)/((P1*0.6+((E1+E2)/2)*0.4)-5.9));
+
+        if(Teste == 0){
+                System.out.print("Digite a nota da prova substitutiva: ");
+                SUB = nota.nextFloat();
+                LPSUB = (float)(LP1+(SUB*0.2));
+                System.out.println("Sua média é:" + LPSUB);
         }else{
-            System.out.println("Sua média é " + lp1);
+                System.out.print("Digite a nota da API: ");
+                API = nota.nextFloat();
+                LPAPI = (float)(LP1+(API*0.5));
+                System.out.println("Sua média é " + LPAPI);
         }
-        
-    }   
-    
-}
+     }
+}  
